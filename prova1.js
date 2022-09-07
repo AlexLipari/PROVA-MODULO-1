003612c134994bdf303dc9b01bc1c0148eb4d192
Alexandre Lipari <santuslipari@gmail.com>
2022-09-06 22:59:38 -0300

    Exercício 4 ordenar valores em ordem crescente

--- prova1.js
+++ prova1.js
@@ -1,10 +1,44 @@
-var numero = parseInt(prompt("insira um número:"))
-console.log (avaliarNumero(numero))
+// var numero = parseInt(prompt("insira um número:"))
+// console.log (avaliarNumero(numero))
 
-function avaliarNumero (numeroParametro){
-    if (numeroParametro% 2 == 0){
-        return true
-    }else {
-        return false
+// function avaliarNumero (numeroParametro){
+//     if (numeroParametro% 2 == 0){
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+// var contador =0
+// var valor = prompt("insira um valor:")
+// console.log("valor inserido:",valor)
+
+// somaDovalor( )
+
+
+// function somaDovalor (valorParametro, somaParametro){
+    
+     
+// }
+let arrayValores =[5,1,8]
+contador =0
+cadastrarNumeros()
+console.log(arrayValores)
+ordenar()
+function cadastrarNumeros(){
+    arrayValores[contador] 
+    contador++
+}
+
+function ordenar() {
+    for (let atual = 0; atual < contador - 1; atual++) {
+        for (let seguinte = atual + 1; seguinte < contador; seguinte++) {
+            let arrayValoresAux   = arrayValores[atual]
+            
+            if ([atual] > [seguinte]) {
+                arrayValores[atual] = arrayValores[seguinte]
+                arrayValores[seguinte] = arrayValoresAux  
+            }
+        }
     }
+    console.log(arrayValores)
 }
