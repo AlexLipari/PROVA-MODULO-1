54fc9b58b37f0625d767499922b485c0bc8f606c
Alexandre Lipari <santuslipari@gmail.com>
2022-09-08 18:10:56 -0300

    Exercício 5 criar uma função para exibir no console, numeros eliminando limiti inferior = 5 e limite superior = 13

--- prova1.js
+++ prova1.js
@@ -19,49 +19,49 @@
     
      
 // }
-let arrayValores =[5,1,8]
-contador =3
+// let arrayValores =[5,1,8]
+// contador =3
 
-console.log(arrayValores)
-ordenarValores()
+// console.log(arrayValores)
+// ordenarValores()
 
-function ordenarValores() {
-    for (let atual = 0; atual < contador - 1; atual++) {
-        for (let seguinte = atual + 1; seguinte < contador; seguinte++) {
-            let arrayValoresAux   = arrayValores[atual]
+// function ordenarValores() {
+//     for (let atual = 0; atual < contador - 1; atual++) {
+//         for (let seguinte = atual + 1; seguinte < contador; seguinte++) {
+//             let arrayValoresAux   = arrayValores[atual]
             
-            if (arrayValores[atual] > array[seguinte]) {
-                arrayValores[atual] = arrayValores[seguinte]
-                arrayValores[seguinte] = arrayValoresAux  
-            }
+//             if (arrayValores[atual] > array[seguinte]) {
+//                 arrayValores[atual] = arrayValores[seguinte]
+//                 arrayValores[seguinte] = arrayValoresAux  
+//             }
+//         }
+//     }
+//     console.log(arrayValores)
+// }
+let arraynumeros = [4,6,7,8,9,10,12,13,14,15]
+let contador = 10
+console.log(arraynumeros)
+analizarValores()
+function analizarValores (parametroInferior,parametroSuperior){
+    for (let index = 0; index < contador; index++){
+        if (arraynumeros[index] <=5){
+            parametroInferior= arraynumeros[index]
+            arraynumeros[index]=0
+            
+        } if(arraynumeros[index] >13){
+            parametroSuperior= arraynumeros[index]
+            arraynumeros[index]=0 
+        }
+
+    }
+    let arraynumeroAux = []
+    for (let index = 0; index < contador; index++){
+        if (arraynumeros[index] !=0){ 
+            arraynumeroAux[index] = arraynumeros[index]
         }
     }
-    console.log(arrayValores)
-}
-// let arraynumeros = [4,6,7,8,9,10,12,13,14,15]
-// let contador = 10
-// console.log(arraynumeros)
-// analizarValores()
-// function analizarValores (parametroInferior,parametroSuperior){
-//     for (let index = 0; index < contador; index++){
-//         if (arraynumeros[index] <=5){
-//             parametroInferior= arraynumeros[index]
-//             arraynumeros[index]=0
-            
-//         } if(arraynumeros[index] >13){
-//             parametroSuperior= arraynumeros[index]
-//             arraynumeros[index]=0 
-//         }
-
-//     }
-//     let arraynumeroAux = []
-//     for (let index = 0; index < contador; index++){
-//         if (arraynumeros[index] !=0){
-//             arraynumeroAux[index] = arraynumeros[index]
-//         }
-//     }
-//     contador--
-//     arraynumeros = arraynumeroAux
-//     console.log(arraynumeros)
+    contador--
+    arraynumeros = arraynumeroAux
+    console.log(arraynumeros)
 }
 
